6d83e57fffc908da854fb1a3234b3008280287e0
luoz <luoz@umich.edu>
2025-07-26 17:57:44 +0800

    checkpoint

--- shared/src/deterministic_rand.rs
+++ shared/src/deterministic_rand.rs
@@ -2,8 +2,10 @@ use std::sync::Mutex;
 
 use lazy_static::lazy_static;
 use rand::{SeedableRng, rngs::StdRng};
+use rand_chacha::ChaCha12Rng;
 
-pub fn create_deterministic_rng() -> StdRng {
+pub fn create_deterministic_rng() -> ChaCha12Rng {
     let seed = 42; // Fixed seed for reproducibility
-    StdRng::seed_from_u64(seed)
+    let rng = ChaCha12Rng::from_seed([seed as u8; 32]);
+    rng
 }
